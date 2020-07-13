8f99f2147bbe156edd441be0c70d15277435826a
e <18392854350@163.com>
2020-07-13 12:53:38 +0800

    master

---
+++ database/migrations/create_admin_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->tinyInteger("status")->default(0);
+            $table->string('invite')->nullable()->comment('邀请码');
+            $table->bigInteger("sid")->nullable()->default(0)->comment('拍店');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_users');
+    }
+}
---
+++ src/Http/Controllers/NavigationController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Moell\LayuiAdmin\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Moell\LayuiAdmin\Http\Requests\Navigation\CreateOrUpdateRequest;
+use Moell\LayuiAdmin\Models\Navigation;
+
+class NavigationController extends Controller
+{
+    /**
+     * @author moell<moell91@foxmail.com>
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        $where = request_intersect(['type', 'guard_name']);
+        if (!isset($where['guard_name']) || !$where['guard_name']) {
+            $where['guard_name'] = 'admin';
+        }
+
+        $navigation = Navigation::query()
+            ->where($where)
+            ->orderBy('sequence', 'desc')
+            ->get()
+            ->toJson();
+
+        return view("admin::navigation.index", compact("navigation"));
+    }
+
+    public function create()
+    {
+        $navigation = Navigation::where('parent_id', 0)->orderBy('sequence','desc')->get();
+        return view("admin::navigation.create", compact("navigation"));
+    }
+
+    public function store(CreateOrUpdateRequest $request)
+    {
+        Navigation::create($request->all());
+
+        return $this->success();
+    }
+
+    public function edit(Navigation $navigation)
+    {
+        $navigation_list = Navigation::where('parent_id', 0)->orderBy('sequence','desc')->get();
+        return view("admin::navigation.edit", compact("navigation", 'navigation_list'));
+    }
+
+    /**
+     * @param CreateOrUpdateRequest $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(CreateOrUpdateRequest $request, $id)
+    {
+        $navigation = Navigation::query()->findOrFail($id);
+
+        $navigation->update($request->toArray());
+
+        return $this->success();
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $navigation = Navigation::query()->findOrFail($id);
+
+        if (Navigation::query()->where('parent_id', $navigation->id)->count()) {
+            return $this->unprocesableEtity([
+                'parent_id' => 'Please delete the subnavigation first.'
+            ]);
+        }
+
+        $navigation->delete();
+
+        return $this->success();
+    }
+}
